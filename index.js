f69ef6d727147c88bd752a667d88f5fc24ff699d
Sourav halder <souravh093@gmail.com>
2023-04-29 16:23:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello bangladesh");
+});
+
+
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
